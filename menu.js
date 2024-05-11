7de6ee530dc165d503691f710493da4165e9987f
RaffaHr <128329385+RaffaHr@users.noreply.github.com>
2024-05-10 21:04:56 -0300

    :bug: fix: tentando resolver o bug no script no input phone

--- menu.js
+++ menu.js
@@ -40,20 +40,20 @@ function toggle() {
 function enviarMensagem() {
     let nome = document.getElementById('name').value
     let email = document.getElementById('email').value
-    let phone = document.getElementById('phone').value
+    let phone = parseFloat(document.getElementById('phone').value)
     let msg = document.getElementById('msg').value
 
-if (phone.length === 0 || phone.trim() === "") {
+if (phone.length === 0) {
     phone = "Não informado";
     return phone
 }
 
     let buildMsg = `Olá Raphael, me chamo ${nome} e vim através do seu portifólio.
-    O motivo do meu contato é: ${msg}.
+O motivo do meu contato é: ${msg}.
     
-    Meios de contato para retorno:
-    Telefone: ${phone}
-    E-mail: ${email}`
+Meios de contato para retorno:
+Telefone: ${phone}
+E-mail: ${email}`
 
     // Construindo o link para a API do WhatsApp com os valores do formulário
     let linkWhatsApp = `https://api.whatsapp.com/send?phone=5527997215329&text=${encodeURIComponent(buildMsg)}`
